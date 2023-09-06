4d8fcb1389dc0c05160c93b92e42740d5426fea8
Cameron Steele <cam.steeleis@gmail.com>
2023-09-06 14:56:58 -0500

    Fix windows support

--- src/helpers/os.ts
+++ src/helpers/os.ts
@@ -1,8 +1,7 @@
-import { platform } from 'os';
+import os, { platform } from 'node:os';
+import { PathLike } from 'node:fs';
+import path from 'node:path';
 import { slicers } from '../constants/slicers';
-import { PathLike } from 'fs';
-import os from 'os';
-import path from 'path';
 
 function expandTilde(filePath: string) {
   if (filePath[0] === '~') {
@@ -11,8 +10,25 @@ function expandTilde(filePath: string) {
   return filePath;
 }
 
+function expandAppData(filePath: string) {
+  return filePath.replace(/%APPDATA%/g, process.env.APPDATA as string);
+}
+
 export function getSlicerPath(): PathLike {
+  const osPlatform = platform() as 'darwin' | 'win32' | 'linux' | string;
+  let slicerPath: string;
+  switch (osPlatform) {
+    case 'darwin':
+    case 'linux':
+      slicerPath = expandTilde(slicers['prusa-slicer'].path[osPlatform]);
+      break;
+    case 'win32':
+      slicerPath = expandAppData(slicers['prusa-slicer'].path[osPlatform]);
+      break;
+    default:
+      throw new Error(`Unsupported platform: ${osPlatform}`);
+  }
   return path.resolve(
-    expandTilde(slicers['prusa-slicer'].path[platform() as 'darwin' | 'win32' | 'linux']),
+    slicerPath
   );
 }
